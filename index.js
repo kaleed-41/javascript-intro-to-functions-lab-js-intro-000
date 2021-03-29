f93acb2f614db751829a84dd6626609c6d1c5bde
Daniel Williams <danielwil621@gmail.com>
2021-03-29 16:42:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string){
   if(lowercase.toLowerCase === lowercase){
     return "I can't hear you!";
   }
-  
+
   if(uppercase.toUpperCase === uppercase){
     return "YES INDEED";
   }
