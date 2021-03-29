365320e0c66e64436647e5c1491c0c4b585f0331
Daniel Williams <danielwil621@gmail.com>
2021-03-29 17:10:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function sayHiToGrandma(string){
     return "YES INDEED!";
   }
 
-  if(console.log(string) === "I love you, Grandma."){
+  if(string === "I love you, Grandma."){
     return "I love you, too.";
   }
 }
